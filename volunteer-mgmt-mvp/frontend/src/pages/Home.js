c5c5f8a9cb52a5623c3ab8cdead3b542fa62da99
ImAluminium <aldricliew1@gmail.com>
2025-09-27 23:00:57 +0800

    Update Home.js

--- volunteer-mgmt-mvp/frontend/src/pages/Home.js
+++ volunteer-mgmt-mvp/frontend/src/pages/Home.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div>
+      <h1>🚲 Ageless Bicyclists Volunteer Portal</h1>
+      <p>Welcome! Sign up, onboard, and start contributing as a volunteer.</p>
+    </div>
+  );
+}
