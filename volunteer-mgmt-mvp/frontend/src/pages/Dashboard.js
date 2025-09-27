6d6e590d8c29dd8f474ccf61233b4f98d158843f
ImAluminium <aldricliew1@gmail.com>
2025-09-27 23:02:03 +0800

    Update Dashboard.js

--- volunteer-mgmt-mvp/frontend/src/pages/Dashboard.js
+++ volunteer-mgmt-mvp/frontend/src/pages/Dashboard.js
@@ -1,1 +1,10 @@
-
+export default function Dashboard() {
+  return (
+    <div>
+      <h2>My Volunteer Dashboard</h2>
+      <p>Hours Contributed: 10</p>
+      <p>Roles Taken: Cycling Buddy, Event Helper</p>
+      <p>Passion Index: ⭐⭐⭐⭐☆</p>
+    </div>
+  );
+}
